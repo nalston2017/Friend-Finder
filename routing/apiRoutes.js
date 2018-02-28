19bed8efd5cc62c7005cb15ebeb9b9e55fad71be
Nathan <nathanalston@gmail.com>
2018-02-28 16:20:02 -0500

    First Commit

---
+++ routing/apiRoutes.js
@@ -0,0 +1,16 @@
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+//////////////////////
+/////ROUTING/////////
+/////////////////////
+
+module.exports = function(app){
+// Get routes for api/friends
+app.get("/api/friends", function(req, res) {
+res.sendFile(path.join(__dirname, "../app/friends.js"));))
+});
+
+
+}
