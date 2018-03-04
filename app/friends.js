dbc1c8f858a3ce6c03ab4ee36a855a505adce33b
Nathan <nathanalston@gmail.com>
2018-03-04 00:11:26 -0500

    Worked on HTML

--- app/friends.js
+++ app/friends.js
@@ -1,2 +1,43 @@
 // Potential friends array
-var friendsArray = []
+var friendsArray = [
+  {
+    "name": "Peter Paker",
+    "photo": "http://purefandom-prod.objects-us-west-1.dream.io/wp-content/uploads/2016/12/peter-parker-1.jpg",
+    "scores": []
+  },
+  {
+    "name": "Peter Paker",
+    "photo": "http://purefandom-prod.objects-us-west-1.dream.io/wp-content/uploads/2016/12/peter-parker-1.jpg",
+    "scores": []
+  },
+  {
+    "name": "Peter Paker",
+    "photo": "http://purefandom-prod.objects-us-west-1.dream.io/wp-content/uploads/2016/12/peter-parker-1.jpg",
+    "scores": []
+  },
+  {
+    "name": "Peter Paker",
+    "photo": "http://purefandom-prod.objects-us-west-1.dream.io/wp-content/uploads/2016/12/peter-parker-1.jpg",
+    "scores": []
+  },
+  {
+    "name": "Peter Paker",
+    "photo": "http://purefandom-prod.objects-us-west-1.dream.io/wp-content/uploads/2016/12/peter-parker-1.jpg",
+    "scores": []
+  },
+  {
+    "name": "Peter Paker",
+    "photo": "http://purefandom-prod.objects-us-west-1.dream.io/wp-content/uploads/2016/12/peter-parker-1.jpg",
+    "scores": []
+  },
+  {
+    "name": "Peter Paker",
+    "photo": "http://purefandom-prod.objects-us-west-1.dream.io/wp-content/uploads/2016/12/peter-parker-1.jpg",
+    "scores": []
+  },
+  {
+    "name": "Peter Paker",
+    "photo": "http://purefandom-prod.objects-us-west-1.dream.io/wp-content/uploads/2016/12/peter-parker-1.jpg",
+    "scores": []
+  }
+]
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,6 +1,7 @@
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
 var path = require("path");
+var friendsArray = require("./app/friends.js");
 
 //////////////////////
 /////ROUTING/////////
@@ -29,16 +30,16 @@ console.log(user);
 
 var difference = ;
 
-for (var i = 0; i < path.length; i++) {
+for (var i = 0; i < friendsArray.length; i++) {
 // Set the difference in scores back to 0 everytime this loops.
 difference = 0;
-console.log("Friend Name: " + path[i].name);
-for (var j = 0; j < path[i].scores.length; j++) {
+console.log("Friend Name: " + friendsArray[i].name);
+for (var j = 0; j < friendsArray[i].scores.length; j++) {
 // Calculate the differences in scores.
-difference += Math.abs(parseInt(userScores[j]) - parseInt(path[i].scores[j]));
+difference += Math.abs(parseInt(userScores[j]) - parseInt(friendsArray[i].scores[j]));
 if (difference < friendMatch.scoreDifference) {
-  friendMatch.name = path[i].name;
-  friendMatch.photo = path[i].photo;
+  friendMatch.name = friendsArray[i].name;
+  friendMatch.photo = friendsArray[i].photo;
   friendMatch.scoreDifference = difference;
   console.log("Your Match: " + perfectFriend.name);
 }
@@ -46,7 +47,7 @@ if (difference < friendMatch.scoreDifference) {
 console.log("Difference in Score: " + difference);
 }
 // Push to the the friends.js
-path.push(user);
+friendsArray.push(user);
 // Push to the html page
 res.json(friendMatch);
 });
